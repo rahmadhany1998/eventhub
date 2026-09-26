3c2cd5ffe1108c17d1548e5f826bd8925f3ecb49
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2026-09-26 16:06:15 +0800

    feat: create homepage

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const mysql = require("mysql2");
+const path = require("path");
 require("dotenv").config();
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes, Op } = require("sequelize");
 
 const PORT = process.env.PORT;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+//Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -62,6 +80,7 @@ const Event = sequelize.define(
     description: { type: DataTypes.TEXT, allowNull: false },
     image_path: { type: DataTypes.STRING(255), allowNull: false },
     venue: { type: DataTypes.STRING(255), allowNull: false },
+    city: { type: DataTypes.STRING(100), allowNull: false },
     event_date: { type: DataTypes.DATE, allowNull: false },
     event_end_date: { type: DataTypes.DATE, allowNull: false },
     max_attendees: { type: DataTypes.INTEGER, allowNull: false },
@@ -227,6 +246,71 @@ EventAttachment.belongsTo(Event, {
   onDelete: "RESTRICT",
   onUpdate: "CASCADE",
 });
+
+//Controllers
+
+app.get("/", async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+
+    let cities = [];
+
+    try {
+      const citiesData = await Event.findAll({
+        attributes: [[Sequelize.fn("DISTINCT", sequelize.col("city")), "city"]],
+        where: { is_published: true },
+        order: [["city", "ASC"]],
+      });
+
+      cities = citiesData.map((c) => c.city).filter((city) => city);
+    } catch (error) {
+      console.error("Error Fetching Data Cities ; ", error);
+      cities = ["Jakarta", "Bandung", "Surabaya", "Medan"];
+    }
+
+    const latestEvents = await Event.findAll({
+      where: { is_published: true },
+      include: [Category, User],
+      order: [["created_at", "DESC"]],
+      limit: 6,
+    });
+
+    const upcomingEvents = await Event.findAll({
+      where: { is_published: true, event_date: { [Op.gte]: new Date() } },
+      include: [Category, User],
+      order: [["event_date", "ASC"]],
+      limit: 6,
+    });
+
+    res.render("home", {
+      user: req.session.user,
+      categories,
+      cities: cities,
+      latestEvents,
+      upcomingEvents,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Helper Function for City Image
+app.locals.getCityImage = (city) => {
+  const cityImages = {
+    Jakarta:
+      "https://images.unsplash.com/photo-1635924201021-e8ae80c2bdc0?q=80&w=443&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Bandung:
+      "https://images.unsplash.com/photo-1667843308501-9c99f55e1c0e?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Surabaya:
+      "https://images.unsplash.com/photo-1566176553949-872b2a73e04e?q=80&w=385&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  };
+
+  return (
+    cityImages[city] ||
+    "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  );
+};
 
 //Sync All Table Models
 
